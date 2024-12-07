9fb596d4f6e35816fbce5145a197255d1ae77c40
rishika <124814358+rishika006@users.noreply.github.com>
2024-12-06 22:16:02 -0500

    Update IConversation.java

--- src/IConversation.java
+++ src/IConversation.java
@@ -1,3 +1,14 @@
+/**
+ * CS180 Team Project -- Direct Messaging Platform
+ *
+ * Interface for Conversation
+ *
+ * @author Nicholas Chong, Rishitha Adusumilli, Shaivi Mishra, Hiya Jha
+ *
+ * @version 11/27/2024
+ *
+ */
+
 public interface IConversation {
      boolean getConversationFileName(String username2) ;
      String startConv(User user) ;

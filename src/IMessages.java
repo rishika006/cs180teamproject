966c8bead64dcd9ac45e5f63243d0bd628fa9748
rishika <124814358+rishika006@users.noreply.github.com>
2024-12-06 22:15:51 -0500

    Update IMessages.java

--- src/IMessages.java
+++ src/IMessages.java
@@ -1,3 +1,14 @@
+/**
+ * CS180 Team Project -- Direct Messaging Platform
+ *
+ * Interface for Messages
+ *
+ * @author Nicholas Chong, Rishitha Adusumilli, Shaivi Mishra, Hiya Jha
+ *
+ * @version 11/27/2024
+ *
+ */
+
 public interface IMessages {
 
     boolean hasConversation(String username2);

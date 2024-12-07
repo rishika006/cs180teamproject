6e7ec2458b3f8374f74f39e768aeca042e7821ea
rishika <124814358+rishika006@users.noreply.github.com>
2024-12-06 22:16:15 -0500

    Update IContactsManager.java

--- src/IContactsManager.java
+++ src/IContactsManager.java
@@ -1,3 +1,14 @@
+/**
+ * CS180 Team Project -- Direct Messaging Platform
+ *
+ * Interface for ContactsManager
+ *
+ * @author Nicholas Chong, Rishitha Adusumilli, Shaivi Mishra, Hiya Jha
+ *
+ * @version 11/27/2024
+ *
+ */
+
 public interface IContactsManager {
 
     String displayContacts();

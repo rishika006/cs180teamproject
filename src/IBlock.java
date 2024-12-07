1849d98ba625ae80470f71614bdfd8c0bb6a655f
rishika <124814358+rishika006@users.noreply.github.com>
2024-12-06 22:16:45 -0500

    Update IBlock.java

--- src/IBlock.java
+++ src/IBlock.java
@@ -1,3 +1,14 @@
+/**
+ * CS180 Team Project -- Direct Messaging Platform
+ *
+ * Interface for Block
+ *
+ * @author Nicholas Chong, Rishitha Adusumilli, Shaivi Mishra, Hiya Jha
+ *
+ * @version 11/27/2024
+ *
+ */
+
 public interface IBlock {
     String getBlockedFilename() ;
     String blockUser(String username2) ;

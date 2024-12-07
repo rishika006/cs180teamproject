17b08318e64687ccd319ef2596343af6b560a4c9
rishika <124814358+rishika006@users.noreply.github.com>
2024-12-06 22:16:31 -0500

    Update IContact.java

--- src/IContact.java
+++ src/IContact.java
@@ -1,3 +1,14 @@
+/**
+ * CS180 Team Project -- Direct Messaging Platform
+ *
+ * Interface for Contact
+ *
+ * @author Nicholas Chong, Rishitha Adusumilli, Shaivi Mishra, Hiya Jha
+ *
+ * @version 11/27/2024
+ *
+ */
+
 public interface IContact {
 
     String getName();

eceba051b187ef9522b445c975af2f4e82fefe0f
rishika <124814358+rishika006@users.noreply.github.com>
2024-12-06 22:15:16 -0500

    Update IUser.java

--- src/IUser.java
+++ src/IUser.java
@@ -1,5 +1,16 @@
 import java.awt.image.BufferedImage;
 
+/**
+ * CS180 Team Project -- Direct Messaging Platform
+ *
+ * Interface for UserManager
+ *
+ * @author Nicholas Chong, Rishitha Adusumilli, Shaivi Mishra, Hiya Jha
+ *
+ * @version 11/27/2024
+ *
+ */
+
 public interface IUser {
     Messages getMessages() ;
     Conversation getConversation() ;
